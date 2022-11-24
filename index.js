0c7e256c9f8968059d75a2e1cd597a4f22a12180
regular-meet-8 <eddiekim444@gmail.com>
2022-11-24 12:00:09 +0300

    done controlflow lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  
+  if (feet<=400){
+    return `This one is on me!`
+
+  }else if (feet>2000){
+    return `I will gladly take your thirty bucks.`
+  }else if(feet>2500){
+    return `No can do.`
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  
+ return city=`NYC`? `Ok, sounds good.` :`No go.`
+
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+
+  switch(tip){
+    case `generous`:
+      return`Thank you so much.`
+break;
+case`not as generous`:
+return`Thank you.`
+break;
+default: 
+return`Bye`
+
+  }
   // Write your code here!
 }
